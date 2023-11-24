769471105bb066d1f7238fcfdb4af5586f615101
dkaszyns <01169181@pw.edu.pl>
2023-11-24 01:08:08 +0100

    Ui

--- annotator_core/src/main/java/com/annotator/core/service/CalculatorService.java
+++ annotator_core/src/main/java/com/annotator/core/service/CalculatorService.java
@@ -1,16 +1,21 @@
 package com.annotator.core.service;
 
+import com.annotator.core.domain.AnnotationResult;
 import com.annotator.core.domain.Calculation;
 import com.annotator.core.kafka.CalculationProducer;
+import com.annotator.core.repository.AnnotationsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class CalculatorService {
     private final CalculationProducer calculationProducer;
+    private final AnnotationsRepository annotationsRepository;
 
     public void requestCalculation(Calculation calculation) {
         try {
@@ -19,5 +24,11 @@ public class CalculatorService {
         } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public List<AnnotationResult> listCalculations() {
+        List<AnnotationResult> results = new ArrayList<>();
+        annotationsRepository.findAll().forEach(results::add);
+        return results;
     }
 }
--- annotator_core/src/main/java/com/annotator/core/web/CalculationController.java
+++ annotator_core/src/main/java/com/annotator/core/web/CalculationController.java
@@ -1,11 +1,15 @@
 package com.annotator.core.web;
 
+import com.annotator.core.domain.AnnotationResult;
 import com.annotator.core.domain.Calculation;
 import com.annotator.core.service.CalculatorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,5 +19,10 @@ public class CalculationController {
     @PostMapping("/calculate")
     public void receiveCalculation(@RequestBody Calculation calculation) {
         calculatorService.requestCalculation(calculation);
+    }
+
+    @GetMapping("/list")
+    public List<AnnotationResult> listCalculations() {
+        return calculatorService.listCalculations();
     }
 }
