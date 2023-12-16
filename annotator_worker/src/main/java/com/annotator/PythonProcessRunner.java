309cc3b7a92574383f9c6be906629a0b0317fee6
Marcin Jarczewski <marcin.jarc@gmail.com>
2023-12-16 19:42:04 +0100

    fix: local development

--- annotator_worker/src/main/java/com/annotator/PythonProcessRunner.java
+++ annotator_worker/src/main/java/com/annotator/PythonProcessRunner.java
@@ -1,7 +1,9 @@
 package com.annotator;
 
-import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+@Slf4j
 public class PythonProcessRunner {
     public Process runPangolinProcess(String inputFile, String outputFile) throws IOException {
         String pwd = System.getProperty("user.dir");
@@ -9,13 +11,14 @@ public class PythonProcessRunner {
         String[] command = {
 //                For local development
                 pwd + "/../venv/bin/pangolin",
-                "pangolin",
+//                For docker
+//                "pangolin",
                 pwd + "/input/" + inputFile,
                 pwd + "/data/GRCh37.primary_assembly.genome.fa.gz",
                 pwd + "/data/gencode.v38lift37.annotation.db",
                 pwd + "/output/" + outputFile
         };
-
+        log.info("Created a process with command: `{}`", String.join(" ", command));
         ProcessBuilder pb = new ProcessBuilder(command);
         pb.redirectErrorStream(true);
         return pb.start();
